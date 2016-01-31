ff3ab5327e2a841498a6d1a9990e2df4dbca6e49
David Oates <deoates@gmail.com>
2016-01-31 11:02:04 -0800

    this works

--- scripts.js
+++ scripts.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
   // smooth animate to link tags
-  $('a').click(function(){
-      $('html, body').animate({
-          scrollTop: $( $.attr(this, 'href') ).offset().top
-      }, 500);
-      return false;
-  });
-
+  if ($('body').hasClass('home')) {
+    $('a').click(function(e){
+        var href = $.attr(this, 'href')
+        if (href.indexOf('/#') > -1) {
+          e.preventDefault();
+          href = href.substr(1)
+        }
+        $('html, body').animate({
+            scrollTop: $( href ).offset().top
+        }, 500);
+        return false;
+    });
+  }
   $("nav select").change(function() {
     window.location = $(this).find("option:selected").attr('data-href');
   });
