4891b68d2a774aaa6fbe20493d2ca32d0584e903
David Oates <deoates@gmail.com>
2016-01-31 11:05:32 -0800

    query by cat slug

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
           <h1>Inbound Sales</h1>
         </a>
         <ul class="post-listing">
-          <?php query_posts('cat=3'); ?>
+          <?php query_posts( array ( 'category_name' => 'inbound-sales', 'posts_per_page' => 3 ) ); ?>
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
               <li>
                 <a href="<?php the_permalink(); ?>">
@@ -117,7 +117,7 @@
           <h1>Inbound Sales</h1>
         </a>
         <ul class="post-listing">
-          <?php query_posts('cat=3'); ?>
+          <?php query_posts( array ( 'category_name' => 'inbound-sales', 'posts_per_page' => 3 ) ); ?>
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
               <li>
                 <a href="<?php the_permalink(); ?>">
@@ -137,7 +137,7 @@
           <h1>Inbound Sales</h1>
         </a>
         <ul class="post-listing">
-          <?php query_posts('cat=3'); ?>
+          <?php query_posts( array ( 'category_name' => 'inbound-sales', 'posts_per_page' => 3 ) ); ?>
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
               <li>
                 <a href="<?php the_permalink(); ?>">
